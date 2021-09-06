c3180a7f69596770c2a3fc6cf2e71979cc63853a
maicolvn1399 <55199282+maicolvn1399@users.noreply.github.com>
2021-09-05 23:44:37 -0600

    [Update] .jar and JUnit test have been created

---
+++ src/test/java_ci_pipeline_demo_JUnit/JUnitTest__1.java
@@ -0,0 +1,14 @@
+package java_ci_pipeline_demo_JUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class JUnitTest__1 {
+
+
+    @Test
+    void name() {
+    }
+}
